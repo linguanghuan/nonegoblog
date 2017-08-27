942dc37de09e7d1540404fd4d96c6a183b967625
linguanghuan <linguanghuan@gmail.com>
2017-08-27 22:56:43 +0800

    mybatis maven 自动生成

---
+++ src/main/java/net/nonego/blog/controller/CatagoryController.java
@@ -0,0 +1,24 @@
+package net.nonego.blog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import net.nonego.blog.inter.CategoryMapper;
+import net.nonego.blog.model.Category;
+
+@Controller
+public class CatagoryController {
+	@Autowired
+	CategoryMapper catagoryMapper;
+	
+	@RequestMapping("/add")
+	@ResponseBody
+	public String add() {
+		Category record = new Category();
+		record.setName("test");
+		catagoryMapper.insert(record );
+		return "success";
+	}
+}
