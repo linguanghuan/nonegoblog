cc298a2f1c1bfc3bf37b509003223eb2390af7b6
linguanghuan <linguanghuan@gmail.com>
2017-08-27 23:51:30 +0800

    thymeleaf

---
+++ src/main/java/net/nonego/blog/controller/IndexController.java
@@ -0,0 +1,14 @@
+package net.nonego.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	@RequestMapping("/")
+	public String index(Model model) {
+		model.addAttribute("user", "test");
+		return "index";
+	}
+}
